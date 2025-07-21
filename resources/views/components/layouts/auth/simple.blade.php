e17a7ee7942cf73bf22d2096946286145a30e2ef
shibi1259 <dev.crm.bi@bluebash.com>
2025-07-21 16:47:38 +0530

    wip

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -7,12 +7,12 @@
 
 <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
     <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-0
-    bg-[url('http://localhost:8000/images/bg1.jpg')] bg-center bg-no-repeat bg-cover">
-        <div class="flex w-full max-w-[800px]  mx-auto flex-col gap-2">
-            <div class="flex flex-col gap-6">
-                {{ $slot }}
-            </div>
+    bg-center bg-no-repeat bg-cover" style="background-image: url('{{ asset('images/bg1.jpg') }}');>
+        <div class=" flex w-full max-w-[800px] mx-auto flex-col gap-2">
+        <div class="flex flex-col gap-6">
+            {{ $slot }}
         </div>
+    </div>
     </div>
     @fluxScripts
 </body>
