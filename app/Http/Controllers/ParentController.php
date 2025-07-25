a7679ec86c2d4b83210a7831dffd6823b4b2a495
shibi1259 <dev.crm.bi@bluebash.com>
2025-07-25 14:56:26 +0530

    wip

--- app/Http/Controllers/ParentController.php
+++ app/Http/Controllers/ParentController.php
@@ -65,7 +65,7 @@ class ParentController extends Controller
         $parent = Auth::user()->parentProfile;
         $bookings = Booking::where('parent_id', $parent->id)
             ->whereNotNull('paid_at')
-            ->where('status', 'accepted')
+            ->where('status', 'completed')
             ->where('paid_out', false)
             ->get();
 
