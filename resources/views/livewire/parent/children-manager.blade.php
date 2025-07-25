e43fe0f272cf86f38448fe7bdbd159a7f1a5ff41
shibi1259 <dev.crm.bi@bluebash.com>
2025-07-25 13:10:58 +0530

    wip

--- resources/views/livewire/parent/children-manager.blade.php
+++ resources/views/livewire/parent/children-manager.blade.php
@@ -106,7 +106,7 @@
             </div>
 
             <div class="mt-6">
-                <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Save Child</button>
+                <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Save</button>
             </div>
         </form>
 
