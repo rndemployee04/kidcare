b8a75271aa388a1bbabdcfe476ec8c0a5ddb1279
shibi1259 <dev.crm.bi@bluebash.com>
2025-07-30 18:15:52 +0530

    wip

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -6,13 +6,13 @@
 </head>
 
 <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-    <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-0
-    bg-center bg-no-repeat bg-cover" style="background-image: url('{{ asset('images/bg1.jpg') }}');>
-        <div class=" flex w-full max-w-[800px] mx-auto flex-col gap-2">
-        <div class="flex flex-col gap-6">
-            {{ $slot }}
+    <div class="flex min-h-screen flex-col items-center justify-center gap-6 p-6 md:p-0 bg-center bg-no-repeat bg-cover"
+        style="background-image: url('{{ asset('images/bg1.jpg') }}');">
+        <div class="w-full max-w-[800px] mx-auto flex flex-col gap-2">
+            <div class="flex flex-col gap-6">
+                {{ $slot }}
+            </div>
         </div>
-    </div>
     </div>
     @fluxScripts
 </body>
